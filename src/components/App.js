f27221cf994d211a77c8ac3301125a6b45b79780
casinca4 <s.antonelli@web.de>
2020-01-06 15:06:44 +0100

    Change App.js, index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import data from '../data/giphy';
 
-function App() {
-    return (
-        <div className="app">
-            
-        </div>
-    )
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { selected: data[0].images.original.url }
+        this.previewChange = this.previewChange.bind(this)
+    }
+
+    previewChange(event) {
+        this.setState({ selected: event.currentTarget.children[1].src })
+    }
+
+    render() {
+        const listItems = data.map(el => {
+            return (
+                <div className="gif-container" key={el.id} onClick={this.previewChange}>
+                    <img src={el.images.original.url}></img>
+                </div>
+            )
+        });
+
+        return (
+            <div className="app">
+                <div className="gallery">
+                    {listItems}
+                </div>
+            </div>
+        )
+    }
 }
 
+
+
 export default App;
+
+// event: Bild
+// currentTarget: wenn man mit der Maus da ist, man nimmt von currentTarget die src
+// children:
+// key: macht, daß React Sachen findet
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-reactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
